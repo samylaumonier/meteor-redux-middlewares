8640e1d01ccd16e6a8c7a5fad5d8eeb9f2389e47
Koleok <kchamb3@gmail.com>
2016-10-28 12:59:18 -0400

    Well gee, i guess i was wrong about not conditionally returning next..

--- src/middlewares/sources.js
+++ src/middlewares/sources.js
@@ -1,22 +1,23 @@
 /* eslint-disable arrow-parens */
+/* eslint-disable consistent-return */
 const computations = {};
 
 export default Tracker => store => next => action => {
-  if (action.meteor && action.meteor.subscribe && action.meteor.get) {
-    // setTimeout is fixing this bug: https://github.com/meteor/react-packages/issues/99
-    setTimeout(() => {
-      if (computations[action.type]) {
-        computations[action.type].stop();
-      }
-
-      computations[action.type] = Tracker.autorun(() => {
-        store.dispatch({
-          type: `${action.type}_CHANGED`,
-          data: action.meteor.get(),
-        });
-      });
-    }, 0);
+  if (!action.meteor || action.meteor.subscribe || !action.meteor.ge) {
+    return next(action);
   }
 
-  return next(action);
+  // setTimeout is fixing this bug: https://github.com/meteor/react-packages/issues/99
+  setTimeout(() => {
+    if (computations[action.type]) {
+      computations[action.type].stop();
+    }
+
+    computations[action.type] = Tracker.autorun(() => {
+      store.dispatch({
+        type: `${action.type}_CHANGED`,
+        data: action.meteor.get(),
+      });
+    });
+  }, 0);
 };
--- src/middlewares/subscriptions.js
+++ src/middlewares/subscriptions.js
@@ -1,42 +1,43 @@
 /* eslint-disable arrow-parens */
+/* eslint-disable consistent-return */
 const subscriptions = {};
 const computations = {};
 
 export default Tracker => store => next => action => {
-  if (action.meteor && action.meteor.subscribe) {
-    // setTimeout is fixing this bug: https://github.com/meteor/react-packages/issues/99
-    setTimeout(() => {
-      if (subscriptions[action.type]) {
-        const subscriptionId = subscriptions[action.type].subscriptionId;
-
-        computations[subscriptionId].stop();
-        subscriptions[action.type].stop();
-      }
-
-      const subscription = action.meteor.subscribe();
-      const subscriptionId = subscription.subscriptionId;
-
-      subscriptions[action.type] = subscription;
-      computations[subscriptionId] = Tracker.autorun(() => {
-        const ready = subscription.ready();
-
-        if (ready) {
-          store.dispatch({
-            type: `${action.type}_CHANGED`,
-            data: action.meteor.get(),
-          });
-        }
-
-        store.dispatch({
-          ready,
-          type: `${action.type}_READY`,
-          data: action.meteor.onReadyData
-            ? action.meteor.onReadyData()
-            : null,
-        });
-      });
-    }, 0);
+  if (!action.meteor || !action.meteor.subscribe) {
+    return next(action);
   }
 
-  return next(action);
+  // setTimeout is fixing this bug: https://github.com/meteor/react-packages/issues/99
+  setTimeout(() => {
+    if (subscriptions[action.type]) {
+      const subscriptionId = subscriptions[action.type].subscriptionId;
+
+      computations[subscriptionId].stop();
+      subscriptions[action.type].stop();
+    }
+
+    const subscription = action.meteor.subscribe();
+    const subscriptionId = subscription.subscriptionId;
+
+    subscriptions[action.type] = subscription;
+    computations[subscriptionId] = Tracker.autorun(() => {
+      const ready = subscription.ready();
+
+      if (ready) {
+        store.dispatch({
+          type: `${action.type}_CHANGED`,
+          data: action.meteor.get(),
+        });
+      }
+
+      store.dispatch({
+        ready,
+        type: `${action.type}_READY`,
+        data: action.meteor.onReadyData
+          ? action.meteor.onReadyData()
+          : null,
+      });
+    });
+  }, 0);
 };
