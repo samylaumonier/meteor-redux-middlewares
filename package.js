d402ba9b5b9432703425a25d0d3c376f1454620f
Samy LAUMONIER <samy.laumonier@gmail.com>
2016-10-30 02:23:19 +0200

    Version 2 Adjust ESLint configuration Publish on npm and atmosphere

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: 'samy:redux-middlewares',
+  version: '2.0.1',
+  summary: 'Middlewares to sync meteor reactive sources with redux store',
+  git: 'https://github.com/samybob1/meteor-redux-middlewares',
+  documentation: 'README.md',
+});
+
+Package.onUse((api) => {
+  api.versionsFrom('1.4.1.1');
+
+  api.use([
+    'ecmascript',
+  ]);
+
+  api.mainModule('src/index.js');
+});
--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const middlewares = [
   sourcesMiddleware,
 ];
 
-const injectTracker =
-  (tracker, middlewares) => middlewares.map(m => m(tracker));
+const injectTracker = (tracker, middlewares) => middlewares.map(m => m(tracker));
 
 export default (tracker) => {
   const [subscriptions, sources] = injectTracker(tracker, middlewares);
