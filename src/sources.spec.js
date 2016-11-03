29f64ac2ab4f66ef243ba580c03d00ff5a1c9af8
Koleok <kchamb3@gmail.com>
2016-11-03 00:30:38 -0400

    Roughly document some mock imports and usage in sources.js spec

--- src/sources.spec.js
+++ src/sources.spec.js
@@ -1,70 +1,17 @@
-// import configureMockStore from 'redux-mock-store';
-//
-// import { REGISTER_REACTIVE_SOURCE, REACTIVE_DATA_CHANGED } from './actions';
-// import sources from './sources';
-//
-//
-// // Used to hold tags pushed from various functions
-// // to ensure the sequence is as expected
-// // (the tags are in pirate speak)
-// const results = [];
-//
-// const treasure = [
-//   { id: 'treasure' },
-// ];
-//
-// const tracker = {
-//   autorun: f => ({
-//     value: f(),
-//     stop: () => results.push('i be stopped now!'),
-//   }),
-// };
-//
-// const payload = {
-//   get() {
-//     results.push('i got me data!');
-//     return treasure;
-//   },
-// };
-//
-// const subscribeAction = {
-//   type: REGISTER_REACTIVE_SOURCE,
-//   payload,
-// };
-//
-// const changedAction = {
-//   type: REACTIVE_DATA_CHANGED,
-//   payload: treasure,
-//   meta: {},
-// };
-//
-// const store = configureMockStore([sources(tracker)])({});
-//
-// const actions = [subscribeAction, subscribeAction];
-// actions.forEach(store.dispatch);
-//
-// it('should handle a subscribe action', () => {
-//   expect(store.getActions()).toEqual([
-//     changedAction,
-//     subscribeAction,
-//     changedAction,
-//     subscribeAction,
-//   ]);
-// });
-//
-// it('should fire functions in payload in the correct sequence', () => {
-//   expect(results).toEqual([
-//     'i got me data!',
-//     'i be stopped now!',
-//     'i got me data!',
-//   ]);
-// });
-
 import configureMockStore from 'redux-mock-store';
 import sources from './sources';
 
-// The results array here is used to hold tags pushed from various functions
-// to ensure the sequence is as expected (the tags are in pirate speak)
+/**
+ * See src/__mocks__/mocks.js
+ *
+ * The results array imported here is used to hold tags pushed from various
+ * functions to ensure the sequence is as expected
+ * (the tags are in pirate speak)
+ *
+ * The two actions are simply calls to the corresponding action creator
+ * functions, and are used as plain objects to dispatch
+ * and later check against
+ */
 const {
   results,
   tracker,
