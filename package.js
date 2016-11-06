8ed86a573b6c8b0cc043469326b4f8727adc69a1
Samy LAUMONIER <samy.laumonier@gmail.com>
2016-11-06 22:11:37 +0100

    Publish version 3.0.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'samy:redux-middlewares',
-  version: '3.0.0',
+  version: '3.0.1',
   summary: 'Middlewares to sync meteor reactive sources with redux store',
   git: 'https://github.com/samybob1/meteor-redux-middlewares',
   documentation: 'README.md',
