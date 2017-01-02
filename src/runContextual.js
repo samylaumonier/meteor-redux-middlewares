4f21d80c5a9413d147ce3c6bdb36f26f0d182b37
Samy LAUMONIER <samy.laumonier@gmail.com>
2017-01-02 10:37:43 +0100

    Remove useless delay parameter

--- src/runContextual.js
+++ src/runContextual.js
@@ -4,7 +4,7 @@ export default run => {
   if (isBrowser()) {
     // setTimeout is fixing this bug:
     // https://github.com/meteor/react-packages/issues/99
-    setTimeout(run, 0);
+    setTimeout(run);
   } else {
     run();
   }
