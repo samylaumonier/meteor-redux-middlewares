05c6fb0370bac6600470e11374bbdfb819bd3125
Samy LAUMONIER <samy.laumonier@gmail.com>
2017-01-05 17:18:30 +0100

    Use Jest fake timers Configure Jest coverage Update dependencies Configure babel to ignore Jest files when we build the package only

--- src/sources.spec.js
+++ src/sources.spec.js
@@ -2,31 +2,36 @@ import configureMockStore from 'redux-mock-store';
 import sources from './sources';
 import { registerReactiveSource } from './actions';
 
+describe('sources middleware', () => {
+  /**
+   * See src/__mocks__/mocks.js
+   *
+   * The results array imported here is used to hold tags pushed from various
+   * functions to ensure the sequence is as expected
+   * (the tags are in pirate speak)
+   *
+   */
+  const {
+    get,
+    results,
+    tracker,
+  } = require('mocks')();
 
-/**
- * See src/__mocks__/mocks.js
- *
- * The results array imported here is used to hold tags pushed from various
- * functions to ensure the sequence is as expected
- * (the tags are in pirate speak)
- *
- */
-const {
-  get,
-  results,
-  tracker,
-} = require('mocks')();
+  const register = registerReactiveSource({ key: 'mates', get });
 
-const register = registerReactiveSource({ key: 'mates', get });
+  const store = configureMockStore([sources(tracker)])({});
 
-const store = configureMockStore([sources(tracker)])({});
-store.dispatch(register);
-store.dispatch(register);
+  store.dispatch(register);
+  jest.runOnlyPendingTimers();
 
-it('should handle a registerReactiveSource action', () => {
-  expect(store.getActions()).toMatchSnapshot();
-});
+  store.dispatch(register);
+  jest.runOnlyPendingTimers();
 
-it('should fire in the correct sequence', () => {
-  expect(results).toMatchSnapshot();
+  it('should handle a registerReactiveSource action', () => {
+    expect(store.getActions()).toMatchSnapshot();
+  });
+
+  it('should fire in the correct sequence', () => {
+    expect(results).toMatchSnapshot();
+  });
 });
